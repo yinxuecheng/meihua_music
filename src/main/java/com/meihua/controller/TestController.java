4e10974c74d71fc667b60eec96969958ee134c20
yingx.xu <yingx.xu@qunar.com>
2022-06-01 16:44:54 +0800

    增加跨域

--- src/main/java/com/meihua/controller/TestController.java
+++ src/main/java/com/meihua/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.meihua.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TestController {
 
     @RequestMapping("/getTest")
